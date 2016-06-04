d6a983eabd805c0fb6bc67d13be247e3209bde30
Christian Höltje <docwhat@gerf.org>
2016-06-04 00:25:30 -0400

    Added more memrecorder tests

--- memrecorder/memrecorder_test.go
+++ memrecorder/memrecorder_test.go
@@ -43,3 +43,58 @@ func TestSawImageTagAt(t *testing.T) {
 		return false
 	})
 }
+
+func TestSawImageTag(t *testing.T) {
+	r := NewMemRecorder()
+
+	tag := "frobnitz"
+
+	r.SawImageTag(tag)
+	r.Sweep(func(found string, _ time.Time) bool {
+		if found != tag {
+			t.Error("I expected to find the tag")
+		}
+		return false
+	})
+}
+
+func TestForget(t *testing.T) {
+	r := NewMemRecorder()
+
+	expectedTag := "frobnitz"
+	unexpectedTag := "foobar"
+
+	r.SawImageTag(expectedTag)
+	r.SawImageTag(unexpectedTag)
+	r.Forget(unexpectedTag)
+	r.Sweep(func(found string, _ time.Time) bool {
+		if found != expectedTag {
+			t.Errorf("I expected to find the tag %v; found %v", expectedTag, found)
+		}
+		return false
+	})
+}
+
+func TestSweep(t *testing.T) {
+	r := NewMemRecorder()
+
+	tag := "frobnitz"
+
+	r.SawImageTag(tag)
+
+	r.Sweep(func(_ string, _ time.Time) bool {
+		return false
+	})
+
+	r.Sweep(func(found string, _ time.Time) bool {
+		if found != tag {
+			t.Error("I expected to find the tag")
+		}
+		return true
+	})
+
+	r.Sweep(func(_ string, _ time.Time) bool {
+		t.Error("I shouldn't find any tags")
+		return false
+	})
+}
